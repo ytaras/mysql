49489e73c5d69a8523a645b7e653dc5444af7daa
Yura Taras <yura.taras@gmail.com>
2013-07-16 16:23:10 +0300

    Receiving packets

--- Mysql.hs
+++ Mysql.hs
@@ -9,16 +9,25 @@ import Control.Monad.Writer
 import Control.Monad.State
 import qualified Data.ByteString as BS
 import Data.Word
+import Data.Bits
+import Bytes
 
-type Log = [String]
+type Log = [LogRecord]
+data LogLevel = Bytes | Structures | Logic
+              deriving Show
+type LogRecord = (LogLevel, String)
+
+logB s = tell [(Bytes, s)]
+logS s = tell [(Structures, s)]
+logL s = tell [(Logic, s)]
+
 type Action = WriterT Log (ReaderT Handle IO)
-type Bytes = BS.ByteString
 
 main = withSocketsDo $ perform
 
 runAction :: Handle -> Action a -> IO a
 runAction h action = runReaderT (runWriterT action) h >>=
-  \(result, log) -> mapM_ putStrLn log >> return result
+  \(result, log) -> mapM_ print log >> return result
 
 port = PortNumber 3306
 
@@ -29,40 +38,41 @@ perform = do
   return handle
 
 mainAction :: Action ()
-mainAction = handshake
+mainAction = handshake >> handshake
 
 handshake = do
-  version <- readWord8
-  stringVersion <- readString
+  ver <- readPacket
+  logL $ "Version is " ++ show ver
   return ()
 
 readBytes :: Int -> Action Bytes
-readBytes n = ask >>= \h -> liftIO $ BS.hGet h n
-
+readBytes n = do
+  h <- ask
+  res <- liftIO $ BS.hGet h n
+  logB $ "Read " ++ show n ++ " bytes: "  ++ show res
+  return res
 
 skip :: Int -> Action ()
 skip n = do
   bytes  <- readBytes n
-  tell $ ["Skipping " ++ (show n) ++  " bytes: " ++ show bytes]
+  logB $ "Skipping " ++ (show n) ++  " bytes: " ++ show bytes
   return ()
 
-readWord8 :: Action Word8
-readWord8 = do
-  bytes <- readBytes 1
-  let ret = BS.head bytes
-  tell $ ["Reading word8 " ++ (show ret) ++ ", it was read from " ++ (show bytes)]
-  return ret
+readWord32 :: Action Word32
+readWord32 = do
+  word32 <- toWord32 `fmap` readBytes 4
+  logS $ "Reading word32 " ++ show word32
+  return word32
 
-readString :: Action Bytes
-readString = evalStateT readString' []
-             where
-               readString' = do
-                 byte <- lift $ readWord8
-                 if byte == 0
-                   then returnString
-                   else modify (byte:) >> readString'
-               returnString = do
-                 bytes <- get
-                 let string = BS.pack $ reverse $ bytes
-                 tell $ ["Reading string " ++ show string ++ " from bytes" ++ show bytes]
-                 return string
+readWord24 :: Action Word32 -- TODO Separate type
+readWord24 = do
+  word24 <- toWord32 `fmap` readBytes 3
+  logS $ "Reading word24 " ++ show word24
+  return word24
+
+readPacket :: Action Bytes
+readPacket = do
+  length <- readWord24
+  logS $ "Received packet length " ++ show length
+  skip 1
+  readBytes $ fromIntegral length
