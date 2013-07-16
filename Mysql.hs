9777b4d61295a17545c90e404666b762f8fbc48a
Yura Taras <yura.taras@gmail.com>
2013-07-16 14:44:05 +0300

    Added ability to read null-terminated string

--- Mysql.hs
+++ Mysql.hs
@@ -6,13 +6,19 @@ import System.IO
 import Control.Monad.Trans
 import Control.Monad.Reader
 import Control.Monad.Writer
-import Data.ByteString as BS
+import Control.Monad.State
+import qualified Data.ByteString as BS
+import Data.Word
 
 type Log = [String]
 type Action = WriterT Log (ReaderT Handle IO)
-
+type Bytes = BS.ByteString
 
 main = withSocketsDo $ perform
+
+runAction :: Handle -> Action a -> IO a
+runAction h action = runReaderT (runWriterT action) h >>=
+  \(result, log) -> mapM_ putStrLn log >> return result
 
 port = PortNumber 3306
 
@@ -23,19 +29,40 @@ perform = do
   return handle
 
 mainAction :: Action ()
-mainAction = do
-  skip 1
-  skip 4
+mainAction = handshake
 
-readBytes :: Int -> Action ByteString
+handshake = do
+  version <- readWord8
+  stringVersion <- readString
+  return ()
+
+readBytes :: Int -> Action Bytes
 readBytes n = ask >>= \h -> liftIO $ BS.hGet h n
 
-runAction :: Handle -> Action a -> IO a
-runAction h action = runReaderT (runWriterT action) h >>=
-  \(result, log) -> print log >> return result
 
 skip :: Int -> Action ()
 skip n = do
   bytes  <- readBytes n
   tell $ ["Skipping " ++ (show n) ++  " bytes: " ++ show bytes]
   return ()
+
+readWord8 :: Action Word8
+readWord8 = do
+  bytes <- readBytes 1
+  let ret = BS.head bytes
+  tell $ ["Reading word8 " ++ (show ret) ++ ", it was read from " ++ (show bytes)]
+  return ret
+
+readString :: Action Bytes
+readString = evalStateT readString' []
+             where
+               readString' = do
+                 byte <- lift $ readWord8
+                 if byte == 0
+                   then returnString
+                   else modify (byte:) >> readString'
+               returnString = do
+                 bytes <- get
+                 let string = BS.pack $ reverse $ bytes
+                 tell $ ["Reading string " ++ show string ++ " from bytes" ++ show bytes]
+                 return string
